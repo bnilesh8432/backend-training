d2923184a4af40df97add193258bc6ef78c024b3
bnilesh8432 <bhoinilesh01@gmail.com.com>
2022-08-12 16:22:14 +0530

    database connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const { default: mongoose } = require('mongoose');
+mongoose.connect("mongodb+srv://functionup:K3OCHkFxsJsV53MA@cluster0.e4rwd2y.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser:true
+})
+.then(() => console.log("MongoDb Is Connected"))
+.catch(err => console.log(err))
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
