cd1633291be1476fd23e4148eee202f00cfc5f9d
bnilesh8432 <bhoinilesh01@gmail.com.com>
2022-08-17 12:20:33 +0530

    db

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const BookController= require("../controllers/bookControllers")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.post('/newBook' ,BookController.newBook);
+router.get('/getAllBooks' ,BookController.allBooks);
 
 module.exports = router;
